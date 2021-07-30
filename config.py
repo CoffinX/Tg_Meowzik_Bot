610b8dcf3cff384700012be74f76aa83c76bf941
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-30 16:37:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ ARQ_API_KEY = "VDFOFE-FLMEKB-LKZYEU-FUUMFU-ARQ" # Get it from @ARQRobot. Needed 
 DeezerQuality = 3 # 1 = 128k Audio Quality
                   # 3 = 320k Audio Quality
                   # 9 = FLAC Format Very High Quality !
-
+session_name = "AQCjzvbzSTOb9HiHQg7xWkIMCP3pplSzk1hl4s-l1hwNfuYiNGwZOdrtrKInWyFEefQxP9_SHLg2z6wSxd5o9LYrRuPp1lZwZb1-gEDkYIoXc1A8P8g_IO7JXKByP7d3zu_YGuEc4oV-ZtLLxDat2YNAmfh8rW7W1WpGs3m9x6KmaQ3_m97Cc1NleUG3EmSANrlRu_QeN4538QmqNdrl0fZ7ZpTJQ4JISzHVH0sLGsr78iS7D8q7ewjVNho2CVvuKDtvD55WyK-DE9klnj4uoIIMS1NlwK9HW559hfvKu-153Si2fs8hT8BbN6BLENbKpG6ouqy3vH8L1ejeq8vGVX4xaCNzhwA"
 # Customization Changes
 Photo_Theme = True    # True - Album Art with Now playing Display
                       # False - No Album Art with Now Playing Display
