02702148b5e05d413a6764f6ce43d10f29228942
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-30 16:32:47 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Change all Values Here Before you use.....
 
-bot_token = "Your_Bot_Token_Here"
-sudo_chat_id = -1234567777
+bot_token = "1925761520:AAHYx8_fq9M18RvOL-cLYLCw2fTOhIyhobU"
+sudo_chat_id = -1001158444924
 
-ARQ_API_KEY = "Place your Key Here -ARQ" # Get it from @ARQRobot. Needed for Saavn and Deezer.
+ARQ_API_KEY = "VDFOFE-FLMEKB-LKZYEU-FUUMFU-ARQ" # Get it from @ARQRobot. Needed for Saavn and Deezer.
 DeezerQuality = 3 # 1 = 128k Audio Quality
                   # 3 = 320k Audio Quality
                   # 9 = FLAC Format Very High Quality !
